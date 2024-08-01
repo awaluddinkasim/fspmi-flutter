40140df6f8b79b6a2fa9cc9729191ebb510df9ea
Awaluddin Kasim <awaluddinkasim7@gmail.com>
2024-08-01 13:08:55 +0800

    one signal

--- lib/main.dart
+++ lib/main.dart
@@ -6,10 +6,21 @@ import 'package:fspmi/cubit/pengaduan_cubit.dart';
 import 'package:fspmi/cubit/register_cubit.dart';
 import 'package:fspmi/pages/loading.dart';
 import 'package:intl/date_symbol_data_local.dart';
+import 'package:onesignal_flutter/onesignal_flutter.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  // Remove this method to stop OneSignal Debugging
+  // OneSignal.Debug.setLogLevel(OSLogLevel.verbose);
+
+  OneSignal.initialize("e76b8dcb-5edf-4fa6-8795-3aa30f88d30e");
+
+  // The promptForPushNotificationsWithUserResponse function will show the iOS or Android push notification prompt. We recommend removing the following code and instead using an In-App Message to prompt for notification permission
+  OneSignal.Notifications.requestPermission(true);
+
   await initializeDateFormatting('id_ID', null);
+
   runApp(const MyApp());
 }
 
